2363078fa8e4741da3407842f5db7847d467cb87
David <davidjx8p@gmail.com>
2017-06-02 18:08:05 +0100

    Some thoughts

---
+++ src/cmd/VMkernel/MLIR/MLIR.h
@@ -0,0 +1,19 @@
+/* Oopsilon
+ * MLIR
+ *
+ *      Copyright Notice
+ *
+ * Copyright (c) 2017 D. Mackay. All rights reserved.
+ *
+ * This file and its contents are supplied under the terms of the Peer
+ * Production Licence as found in the Oopsilon project's source repository,
+ * and David Mackay is the sole copyright holder.
+ *
+ *      End Copyright Notice
+ */
+
+#pragma once
+
+struct Program
+{
+};
